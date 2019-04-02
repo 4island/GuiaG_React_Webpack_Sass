a4be6b2479f19f183bae8ed724e00c61d7b29634
Alan Martin <very.directx@gmail.com>
2019-04-02 01:27:56 -0300

    UI development and engine

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col} from 'react-bootstrap';
+import './Resultado.scss';
 
 export const Resultado = props => {
     const { cliente } = props;
@@ -7,29 +8,28 @@ export const Resultado = props => {
     return (
         <Row className="show-grid text-left">
             <Col xs={12} md={6}>
+                    <p className="card-text">
+                        <strong>
+                        &nbsp;
+                        {cliente.name} - Categoria: {cliente.categoria}   <br />
 
-                        <p className="card-text">
-                            <strong>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            {cliente.name} - Categoria: {cliente.categoria}   <br />
+                        &nbsp;Toca / haz click para acceder a mas info  ↓ <br /><br />
 
-                            &nbsp;Toca / haz click para acceder a mas info  ↓ <br /><br />
+                        
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a className="wtsp-link" href={`https://api.whatsapp.com/send?phone=549${cliente.telefono}&text=Hola%2C%20quiero%20saber%20mas`}>
+                            <i className="fab fa-whatsapp fa-2x"></i>&nbsp;{cliente.telefono} 
+                        </a>
+                        <br /><br />
 
-                            
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <a className="wtsp-link" href={`https://api.whatsapp.com/send?phone=549${cliente.telefono}&text=Hola%2C%20quiero%20saber%20mas`}>
-                                <i className="fab fa-whatsapp fa-2x"></i>&nbsp;{cliente.telefono} 
-                            </a>
-                            <br /><br />
+                        
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a  href={`https://${cliente.web}`}>
+                        <i className="fas fa-link fa-2x"></i>&nbsp;{cliente.web}
+                        </a>
 
-                            
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <a  href={`https://${cliente.web}`}>
-                            <i className="fas fa-link fa-2x"></i>&nbsp;{cliente.web}
-                            </a>
-
-                            </strong>
-                        </p>
+                        </strong>
+                    </p>
             </Col>  
         </Row>
     );
